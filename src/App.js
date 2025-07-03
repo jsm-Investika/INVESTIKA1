c0354069fb0236ecd1618891a5778692d390340e
jsm-Investika <jjsm3826@gmail.com>
2025-07-03 16:57:45 +0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Karibu INVESTIKA</h1>
+      <p>Mfumo wa kidigitali wa uwekaji na uwekezaji wa pesa.</p>
+    </div>
+  );
+}
+
+export default App
